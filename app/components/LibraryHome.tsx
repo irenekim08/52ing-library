8caf0c0e4a555a11adbd11c646d61e38f2145436
IRENE KIM <yellowrose@MacBook-Air-1082.local>
2026-09-10 23:52:57 -0500

    font

--- app/components/LibraryHome.tsx
+++ app/components/LibraryHome.tsx
@@ -84,11 +84,11 @@ export default function LibraryHome({ books }: { books: Book[] }) {
             </h1>
 
             {/* SUBTITLE */}
-            <p className="font-basic mt-4 text-l text-[#1F2A44] sm:text-xl">
+            <p className="mt-4 text-l text-[#1F2A44] sm:text-xl">
               우리들의 새로운 궤도를 찾아서!
             </p>
 
-            <p className="mt-2 text-lg text-[#1F2A44]/80 sm:text-xl">
+            <p className="font-basic mt-2 text-lg text-[#1F2A44]/80 sm:text-xl">
               어린이와 청소년을 위한 한국어 책 도서관
             </p>
 
